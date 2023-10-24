89b8787bc9c8be45ec79868b8ddc3b6642a6de05
EvansRobbie <evansrobbie5311@gmail.com>
2023-10-24 23:54:06 +0300

    banner

--- src/components/nav/HomeBanner.tsx
+++ src/components/nav/HomeBanner.tsx
@@ -1,15 +1,30 @@
+import Image from "next/image";
 import React from "react";
 
 const HomeBanner = () => {
   return (
     <div className=" relative bg-gradient-to-r from-sky-500 via-indigo-500 to-sky-700">
       <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
-        <div>
-          <h1>Summer Sale</h1>
-          <p>Enjoy discounts on selected items</p>
-          <p>GET 50% OFF</p>
+        <div className="mb-8 md:mb-0 text-center">
+          <h1 className="text-4xl md:text-6xl font-bold text-white mb-4 ">
+            Summer Sale
+          </h1>
+          <p className="text-white text-lg md:text-xl mb-2">
+            Enjoy discounts on selected items
+          </p>
+          <p className="text-yellow-500 text-2xl md:text-5xl font-bold">
+            GET 50% OFF
+          </p>
         </div>
-        <div></div>
+        <div className="w-1/3 relative aspect-video">
+          <Image
+            src={"/banner-image.png"}
+            alt="Banner Image"
+            fill
+            priority
+            className="object-contain"
+          />
+        </div>
       </div>
     </div>
   );
